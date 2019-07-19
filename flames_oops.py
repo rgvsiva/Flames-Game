8b2fde36b55b9c1b022e6c98d8862ac8e74cebdc
rgvsiva <52554037+rgvsiva@users.noreply.github.com>
2019-07-19 19:22:56 +0530

    some bugs fixed

--- flames_oops.py
+++ flames_oops.py
@@ -6,20 +6,19 @@
 class flames_game:
     def __init__(self):
         print("*************----FLAMES----**************")
+        self.flames = ['Friend', 'Lover', 'Attraction', 'Marriage', 'Enemy', 'Sibling']
+    def result(self):
+        print("**Length of Name should be below 30(we consider alphabets and numbers only)**")
         self.b=input("Enter Boy's Name: ")
         self.g=input("Enter Girl's Name: ")
-        self.boy=self.remove_spc_char(self.b)
-        self.girl=self.remove_spc_char(self.g)
-        self.total_char()
-        self.flames=['Friend','Lover','Attraction','Marriage','Enemy','Sibling']
-        self.total = len(self.boy) + len(self.girl)
-    #-------------------------------------------
-    def remove_spc_char(self,name):
-        self.lis = []
-        for i in name.lower():
-            if i.isalpha():
-                self.lis.append(i)
-        return self.lis
+        if len(self.b)<=30 and len(self.g)<=30:
+            self.boy = [i for i in self.b.lower() if i.isalnum()]
+            self.girl = [i for i in self.g.lower() if i.isalnum()]
+            self.total_char()
+            self.final_logic()
+            print('Result: ', self.g.capitalize(), ' is "', self.flames[0], '" for ', self.b.capitalize(), '.')
+        else:
+            print("**Length of Name should be below 30(we consider alphabets and numbers only)**")
     #----------------------------------------
     def total_char(self):
         for i in self.boy:
@@ -30,19 +29,13 @@ class flames_game:
     # --------------------------------------------
     def final_logic(self):
         while len(self.flames)>1:
-            v=self.total%len(self.flames)
+            v = len(self.boy+self.girl) % len(self.flames)
             if v==0:
                 self.flames=self.flames[:-1]
                 self.final_logic()
             else:
                 self.flames = self.flames[v:] + self.flames[:v-1]
                 self.final_logic()
-    def result(self):
-        self.final_logic()
-        print( 'Result: ',self.g.capitalize(),' is "',self.flames[0],'" for ',self.b.capitalize(),'.')
-    #------------------------------------
+#--------------------------------------------------------------------
 obj=flames_game()
 obj.result()
-
-
-#
